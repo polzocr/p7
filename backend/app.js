8e625ed976b1726f04efe74dd4433ea0a5978066
Polzocr <micro.cpt.62@outlook.fr>
2022-04-22 10:28:48 +0200

    ajout du cors + express.json()

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); //on recupère le corps JSON des requête POST
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
 module.exports = app;
