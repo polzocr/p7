6fa46eb1b1c36a7c66c7d5a1ed5e5eed0d5b22be
Polzocr <micro.cpt.62@outlook.fr>
2022-04-25 14:12:53 +0200

    ajout des fonction controllers post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -2,12 +2,19 @@ const db = require('../models');
 const fs = require('fs');
 
 exports.createPost = (req, res, next) => {
+    let img;
+    if(req.file){
+        img = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } else {
+        img = null;
+    }
     db.Post.create({
-        UserId: req.body.userId, 
+        UserId: req.auth.userId, 
         name:req.body.name, 
-        text:req.body.text, 
-        image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        //image_url: req.body.image_url
+        text:req.body.text,
+        image_url: img,
+        likes: 0,
+        dislikes: 0
     })
     .then(() => res.status(201).json({message: 'Post crée avec succès !'}))
     .catch(error => {
@@ -17,21 +24,162 @@ exports.createPost = (req, res, next) => {
 };
 
 exports.getAllPosts = (req, res, next) => {
-
+    db.Post.findAll()
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(400).json({error}));
 };
 
 exports.getOnePost = (req, res, next) => {
-
+    db.Post.findOne({where: {id: req.params.id} })
+    .then(post => res.status(200).json(post))
+    .catch(error => res.status(404).json({error}));
 };
 
 exports.modifyPost = (req, res, next) => {
-
+    const postObject = req.file?{
+        ...req.body,
+        image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    }:{...req.body}
+    db.Post.update({ ...postObject}, {where: {id: req.params.id} })
+    .then(() => res.status(200).json({message:'Objet modifié avec succès !'}))
+    .catch(error => {res.status(400).json({error});
+    });
+ 
 };
 
 exports.deletePost = (req, res, next) => {
-
+    let filename = null;
+    db.Post.findOne({where: {id: req.params.id} })
+    .then(post => {
+        if(post.image_url !== null){
+            filename = post.image_url.split('/images/')[1];
+        };
+        fs.unlink(`images/${filename}`, () => {
+            db.Post.findOne({where: {id:req.params.id} })
+            .then(post => {
+                if(!post) {
+                    return res.status(404).json({error: new Error('Objet non trouvé !')});
+                }
+                if(post.userId !== req.auth.userId){
+                    return res.status(401).json({error: new Error('Requête non autorisée !')});
+                }
+                db.Post.destroy({where: {id:req.params.id} })
+                .then(() => res.status(200).json({message: 'Objet supprimé avec succès !'}))
+                .catch(error => res.status(400).json({error:"Erreur ici"}));
+            })
+            .catch(error => res.status(500).json({error: "Erreur la"}));
+        })   
+    })
+    .catch(error => res.status(500).json({error: "Erreur ici ou la"}));
 };
 
 exports.likePost = (req, res, next) => {
 
 };
+
+// const postObject = req.file? 
+//     {   
+//         ...req.body,
+//         image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//     }:{...req.body}
+
+
+
+
+// exports.modifyPost = (req, res, next) => {
+//     db.Post.update({
+//         UserId: req.body.userId, 
+//         name:req.body.name, 
+//         text: req.body.text,
+//         //image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//     }, {where: {id: req.params.id} })
+//     .then(() => res.status(200).json({message:'Objet modifié avec succès !'}))
+//     .catch(error => {
+//         console.log(error);
+//         res.status(400).json({error});
+//     });
+ 
+// };
+
+
+// db.Post.create({
+//         UserId: req.auth.userId, 
+//         name:req.body.name, 
+//         text:req.body.text, 
+//         image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+//         likes: 0,
+//         dislikes: 0
+//     })
+
+
+
+
+// exports.createPost = (req, res, next) => {
+//     const postObject = req.file? {
+//         ...JSON.parse(req.body.post),
+//         image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//     }:{...req.body.post}
+//     db.Post.create({
+        
+//         UserId: req.auth.userId,
+//         ...postObject,
+//         likes: 0,
+//         dislikes: 0
+//     })
+//     .then(() => res.status(201).json({message: 'Post crée avec succès !'}))
+//     .catch(error => {
+//         console.log(error)
+//         res.status(400);
+//     });
+// };
+
+
+
+
+
+
+
+
+// exports.deletePost = (req, res, next) => {
+//     db.Post.findOne({where: {id: req.params.id} })
+//     .then(post => {
+//         if(post.image_url !== null){
+//             const filename = post.image_url.split('/images/')[1];
+//             fs.unlink(`images/${filename}`, () => {
+//             db.Post.findOne({where: {id:req.params.id} })
+//             .then(post => {
+//                 if(!post) {
+//                     return res.status(404).json({error: new Error('Objet non trouvé !')});
+//                 }
+//                 if(post.userId !== req.auth.userId){
+//                     return res.status(401).json({error: new Error('Requête non autorisée !')});
+//                 }
+//                 db.Post.destroy({where: {id:req.params.id} })
+//                 .then(() => res.status(200).json({message: 'Objet supprimé avec succès !'}))
+//                 .catch(error => res.status(400).json({error:"Erreur ici"}));
+//             })
+//             .catch(error => res.status(500).json({error: "Erreur la"}));
+//         })
+//         } else {
+//             const filename = null;
+//             fs.unlink(`images/${filename}`, () => {
+//             db.Post.findOne({where: {id:req.params.id} })
+//             .then(post => {
+//                 if(!post) {
+//                     return res.status(404).json({error: new Error('Objet non trouvé !')});
+//                 }
+//                 if(post.userId !== req.auth.userId){
+//                     return res.status(401).json({error: new Error('Requête non autorisée !')});
+//                 }
+//                 db.Post.destroy({where: {id:req.params.id} })
+//                 .then(() => res.status(200).json({message: 'Objet supprimé avec succès !'}))
+//                 .catch(error => res.status(400).json({error:"Erreur ici"}));
+//             })
+//             .catch(error => res.status(500).json({error: "Erreur la"}));
+//         })
+//         }
+        
+        
+//     })
+//     .catch(error => res.status(500).json({error: "Erreur ici ou la"}));
+// };
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -7,10 +7,10 @@ const multer = require('../middleware/multer');
 const postCtrl = require('../controllers/post');
 
 router.post('/',auth, multer, postCtrl.createPost )
-router.get('/', postCtrl.getAllPosts )
-router.get('/:id', postCtrl.getOnePost )
-router.put('/:id', postCtrl.modifyPost )
-router.delete('/:id', postCtrl.deletePost )
-router.post('/:id/like', postCtrl.likePost )
+router.get('/', auth, postCtrl.getAllPosts )
+router.get('/:id', auth, postCtrl.getOnePost )
+router.put('/:id', auth, multer, postCtrl.modifyPost )
+router.delete('/:id', auth,postCtrl.deletePost )
+router.post('/:id/like',auth, postCtrl.likePost )
 
 module.exports = router;
