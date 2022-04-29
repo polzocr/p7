69b7ddb21c0a4efca566a97fccf5690f56cd1c5e
Polzocr <micro.cpt.62@outlook.fr>
2022-04-29 20:02:41 +0200

    debut de la requete signup

--- frontend/vue-frontend/src/store/index.js
+++ frontend/vue-frontend/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+const axios = require('axios').default;
 
 Vue.use(Vuex)
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/',
+});
 
 export default new Vuex.Store({
   state: {
@@ -9,8 +14,24 @@ export default new Vuex.Store({
   getters: {
   },
   mutations: {
+   
   },
   actions: {
+    postRequest(context , userInfo){
+      context.commit;
+      instance.post('/signup', {
+        firstName: userInfo.firstName,
+        lastName: userInfo.lastName,
+        email: userInfo.email,
+        password: userInfo.password
+      })
+    .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+    }
   },
   modules: {
   }
--- frontend/vue-frontend/src/views/SignupView.vue
+++ frontend/vue-frontend/src/views/SignupView.vue
@@ -1,17 +1,52 @@
 <template>
-   <div>
-     <SignupComp/>
-   </div>
+   <div class="signup">
+        <div class="form">
+            <div class="">
+                <label for="lastName">Nom</label>
+                <input v-model="lastName" type="text" name="lastName">
+            </div>
+            <div class="">
+                <label for="firstName">Prénom</label>
+                <input v-model="firstName" type="text" name="firstName">
+            </div>
+            <div class="">
+                <label for="email">Email</label>
+                <input v-model="email" type="email" name="email">
+            </div>
+            <div class="">
+                <label for="password">Mot de passe</label>
+                <input v-model="password" type="password" name="password">   
+            </div>
+            <div class="">
+                <button @click="SignupRequest">S'enregistrer</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import SignupComp from '@/components/SignupComp.vue'
+
 
 export default {
   name: 'SignupView',
-   components: {
-      SignupComp
-   }
+  data: function(){ 
+          return {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: ''
+          }
+        },
+  methods:{
+      SignupRequest(){
+          this.$store.dispatch('postRequest', {
+              lastName: this.lastName,
+              firstName: this.firstName,
+              email: this.email,
+              password: this.password
+          })
+      }
+  }
 }
 </script>
